f0c84ff2f79a3ca424462ed4088dc1ea605db170
iamnader <iamnader@gmail.com>
2022-09-01 15:29:40 +0100

    loop through full sequence

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,34 @@
 import * as dotenv from 'dotenv';
+
+import { minBonus, parseUnhealthyLoans } from './aave.js';
+import { setTokenList } from './chains.js';
+import { knownTokens, liquidationProfits } from './liquidations.js';
 import { getLoans } from './theGraph.js';
+import { getGas } from './utils/gas.js';
 dotenv.config();
 
 run();
 
-//infinite loop calling fetchUnhealthyLoans
-//sleep for 1 minute before each call
+//infinite loop calling checking for profitable loans to liquidate
 async function run(): Promise<void> {
-  // while (true) {
-  console.log('fetching loans');
+  console.log(`Fetching loans from: ${process.env.CHAIN}`);
 
-  const users = await getLoans(1000);
-  console.log(users);
+  await getGas(); // get this ahead of time, as the value is good for each loop
+  setTokenList(); // relies on env vars so needs to be run at runtime
 
-  // await sleep(60000);
-  // }
-  //TODO calculate liquidation threshold daily
+  /* eslint-disable no-constant-condition */
+  while (true) {
+    const users = await getLoans(1000);
+    const unhealthyLoans = parseUnhealthyLoans(users);
+    const minBonusLoans = minBonus(unhealthyLoans);
+    const knownTokenLoans = knownTokens(minBonusLoans);
+    const profitableLoans = await liquidationProfits(knownTokenLoans);
+
+    profitableLoans.forEach((l) => console.log(l));
+    await sleep(60000);
+  }
 }
 
-// function sleep(ms: number) {
-//   return new Promise((resolve) => setTimeout(resolve, ms));
-// }
+function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
