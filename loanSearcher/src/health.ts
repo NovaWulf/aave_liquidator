b3c40b2de559cd89602fed2a76d40c43b1f2fa31
iamnader <iamnader@gmail.com>
2022-10-11 16:39:34 +0100

    simpel script to test HF for user

---
+++ loanSearcher/src/health.ts
@@ -0,0 +1,18 @@
+import * as dotenv from 'dotenv';
+
+import { getUserHealthFactor } from './aaveHealth.js';
+
+dotenv.config();
+
+run();
+
+//infinite loop calling checking for profitable loans to liquidate
+async function run(): Promise<void> {
+  const hf = await getUserHealthFactor(
+    process.env.TEST_USER_TO_LIQUIDATE,
+    parseInt(process.env.TEST_BLOCK_NUMBER),
+  );
+  console.log(hf);
+
+  console.log('Health Factor: ' + hf.healthFactor);
+}
