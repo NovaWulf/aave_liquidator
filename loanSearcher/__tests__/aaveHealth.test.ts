0cacc821b81da6be58dfb6045c8561bdad495704
iamnader <iamnader@gmail.com>
2022-10-11 16:40:04 +0100

    update health test to test specific user

--- loanSearcher/__tests__/aaveHealth.test.ts
+++ loanSearcher/__tests__/aaveHealth.test.ts
@@ -1,24 +1,30 @@
-import { parseUnhealthyLoans } from '../src/aave.js';
+import { mapLoans } from '../src/aave.js';
 import { getUserHealthFactor } from '../src/aaveHealth.js';
-import { getLoans } from '../src/theGraph.js';
+import { getUserLoans } from '../src/theGraph.js';
 
 describe('live loans', () => {
+  // compare our calculation of a health factor from thegraph data to the onchain one
   it('calculates health within 1%', async () => {
-    const loans = await getLoans(100, 1);
-    const unhealthy = parseUnhealthyLoans(loans);
-    const aaveSummary = await getUserHealthFactor(unhealthy[0].userId);
+    const loans = await getUserLoans(
+      process.env.TEST_USER_TO_LIQUIDATE,
+      parseInt(process.env.TEST_BLOCK_NUMBER),
+    );
+    const mappedLoan = mapLoans(loans)[0];
+
+    const aaveSummary = await getUserHealthFactor(
+      mappedLoan.userId,
+      parseInt(process.env.TEST_BLOCK_NUMBER),
+    );
 
     let healthFactorDiff = 0;
 
-    if (unhealthy[0]) {
-      healthFactorDiff = Math.abs(
-        unhealthy[0]['healthFactor'] - parseFloat(aaveSummary['healthFactor']),
-      );
-      console.log(`health factor diff = ${healthFactorDiff}`);
-      if (healthFactorDiff > 0.01) {
-        console.log(unhealthy[0]);
-        console.log(aaveSummary);
-      }
+    healthFactorDiff = Math.abs(
+      mappedLoan['healthFactor'] - parseFloat(aaveSummary['healthFactor']),
+    );
+    console.log(`health factor diff = ${healthFactorDiff}`);
+    if (healthFactorDiff > 0.01) {
+      console.log(mappedLoan);
+      console.log(aaveSummary);
     }
 
     expect(healthFactorDiff < 0.01).toBeTruthy();
