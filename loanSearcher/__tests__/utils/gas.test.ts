f1582a0edd1cbf995f4802ce7484cd6b49494577
iamnader <iamnader@gmail.com>
2022-10-13 13:22:21 +0100

    allow for longer gas test

--- loanSearcher/__tests__/utils/gas.test.ts
+++ loanSearcher/__tests__/utils/gas.test.ts
@@ -1,7 +1,10 @@
 import { getGas } from '../../src/utils/gas.js';
+import { jest } from '@jest/globals';
 
 describe('getGas', () => {
   it('returns gas price', async () => {
+    jest.setTimeout(10000);
+
     const gas = await getGas();
     expect(gas).toBeGreaterThan(5);
   });
