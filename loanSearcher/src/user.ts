f8cc3a0d80e2a821ca83c2f1409147f24d5dcd39
iamnader <iamnader@gmail.com>
2022-10-17 14:34:25 +0100

    reduce logging

--- loanSearcher/src/user.ts
+++ loanSearcher/src/user.ts
@@ -14,12 +14,9 @@ export async function run(): Promise<void> {
   setTokenList(); // relies on env vars so needs to be run at runtime
 
   await getGas(); // get this once per loop
-  const userLoans = await getUserLoans(
-    process.env.TEST_USER_TO_LIQUIDATE,
-    parseInt(process.env.TEST_BLOCK_NUMBER),
-  );
-  console.log(`user: ${JSON.stringify(userLoans, null, 2)}`);
-  const loan = await findProfitableLoan(userLoans);
-
-  console.log('most profitable loan: ' + loan);
+  const userLoans = await getUserLoans({
+    userId: process.env.TEST_USER_TO_LIQUIDATE,
+    blockNumber: parseInt(process.env.TEST_BLOCK_NUMBER),
+  });
+  await findProfitableLoan(userLoans);
 }
